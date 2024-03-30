1d71d444893b0d8d6e6b68b40be37861cd869e14
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2024-03-30 11:36:40 +0500

    ADD: validators

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from habits.models import Habit
+from habits.validators import RewardAndHabitValidator, ActionTimeValidator, IsNiceValidator, NiceHabitValidator, PeriodValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+
+        validators = [
+            RewardAndHabitValidator(reward='reward', habit='associated_hab'),
+            ActionTimeValidator(action_time='action_time'),
+            IsNiceValidator(habit='associated_hab'),
+            NiceHabitValidator(habit_is_nice='is_nice', reward='reward', habit='associated_hab'),
+            PeriodValidator(period='period')
+        ]
---
+++ habits/validators.py
@@ -0,0 +1,59 @@
+from rest_framework.exceptions import ValidationError
+
+
+class RewardAndHabitValidator:
+    def __init__(self, reward, habit):
+        self.reward = reward
+        self.habit = habit
+
+    def __call__(self, value):
+        reward = dict(value).get(self.reward)
+        habit = dict(value).get(self.habit)
+        if reward and habit:
+            raise ValidationError('Нельзя одновременно выбирать связанную привычку и вознаграждение')
+
+
+class ActionTimeValidator:
+    def __init__(self, action_time):
+        self.action_time = action_time
+
+    def __call__(self, value):
+        action_time = dict(value).get(self.action_time)
+        if action_time > 120:
+            raise ValidationError('Время выполнения привычки не должно быть больше 120 секунд')
+
+
+class IsNiceValidator:
+    def __init__(self, habit):
+        self.habit = habit
+
+    def __call__(self, value):
+        if value.get(self.habit):
+            is_nice = dict(value).get(self.habit).is_nice
+            if not is_nice:
+                raise ValidationError('В связанные привычки могут попадать только привычки с признаком приятной привычки')
+
+
+class NiceHabitValidator:
+    def __init__(self, habit_is_nice, reward, habit):
+        self.habit_is_nice = habit_is_nice
+        self.reward = reward
+        self.habit = habit
+
+    def __call__(self, value):
+        habit_is_nice = dict(value).get(self.habit_is_nice)
+        reward = dict(value).get(self.reward)
+        habit = dict(value).get(self.habit)
+        if habit_is_nice:
+            if reward or habit:
+                raise ValidationError('У приятной привычки не может быть вознаграждения или связанной привычки')
+
+
+class PeriodValidator:
+    def __init__(self, period):
+        self.period = period
+
+    def __call__(self, value):
+        period = dict(value).get(self.period)
+        if period < 1:
+            raise ValidationError('Нельзя выполнять привычку реже, чем 1 раз в 7 дней')
